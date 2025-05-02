ba351cd51d958ec1fe6fa1b753f4505841310b5b
parkstar12 <zzanggu12280@naver.com>
2025-05-02 15:43:09 +0900

    Revert "Reapply "Merge branch 'parkstar'""
    This reverts commit 22435b72592410fbbdfaba99bd2958b5ab09062c.

---
+++ back-end/src/main/java/com/megacoffee/kiosk/auth/application/AuthService.java
@@ -0,0 +1,4 @@
+package com.megacoffee.kiosk.auth.application;
+
+public class AuthService {
+}
--- back-end/src/main/java/com/megacoffee/kiosk/member/application/port/in/GetMember.java
+++
@@ -1,9 +0,0 @@
-package com.megacoffee.kiosk.member.application.port.in;
-
-import com.megacoffee.kiosk.member.domain.Member;
-
-import java.util.UUID;
-
-public interface GetMember {
-    Member exec(UUID memberId);
-}
